be7898e3720ee13cc1c720a8980144bfd9296f07
Kush <Kush@KUSH>
2018-01-06 19:33:12 +0530

    added tripper app project with some skeleton classes

---
+++ tripper/src/main/java/com/kush/tripper/location/Location.java
@@ -0,0 +1,5 @@
+package com.kush.tripper.location;
+
+public interface Location {
+
+}
---
+++ tripper/src/main/java/com/kush/tripper/trip/Trip.java
@@ -0,0 +1,28 @@
+package com.kush.tripper.trip;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.kush.tripper.location.Location;
+import com.kush.tripper.place.Place;
+
+public class Trip {
+
+    public void setName(String name) {
+    }
+
+    public void setDurationInDays(int days) {
+    }
+
+    public void setStartTime(LocalDateTime time) {
+    }
+
+    public void setStartingPoint(Location location) {
+    }
+
+    public void setReturnPoint(Location location) {
+    }
+
+    public void setPlacesToVisit(List<Place> places) {
+    }
+}
