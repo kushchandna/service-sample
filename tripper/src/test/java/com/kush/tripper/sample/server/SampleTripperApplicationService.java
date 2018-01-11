f0b123fb2faece475955e46298f84b6c141fc33b
Kush <Kush@KUSH>
2018-01-11 13:01:14 +0530

    added functions in sample tripper services

--- tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperApplicationService.java
+++ tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperApplicationService.java
@@ -1,6 +1,32 @@
 package com.kush.tripper.sample.server;
 
+import static com.kush.utils.id.Identifier.id;
+
 import com.kush.lib.service.server.api.BaseService;
+import com.kush.tripper.itinerary.Itinerary;
+import com.kush.tripper.place.Place;
+import com.kush.tripper.trip.Trip;
+import com.kush.utils.id.Identifier;
 
 public class SampleTripperApplicationService extends BaseService {
+
+    public Identifier saveTrip(Trip trip) {
+        return id("Random ID");
+    }
+
+    public Place findPlace(String placeName) {
+        return new Place() {
+        };
+    }
+
+    public Itinerary getItinerary(Identifier id) {
+        return new Itinerary() {
+        };
+    }
+
+    public Identifier[] getAllTrips() {
+        Identifier[] ids = new Identifier[1];
+        ids[0] = id("Random ID");
+        return ids;
+    }
 }
--- tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperUserService.java
+++ tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperUserService.java
@@ -1,15 +1,22 @@
 package com.kush.tripper.sample.server;
 
 import com.kush.lib.service.server.api.BaseService;
+import com.kush.logger.Logger;
+import com.kush.logger.LoggerFactory;
 
 public class SampleTripperUserService extends BaseService {
 
+    private static final Logger LOGGER = LoggerFactory.INSTANCE.getLogger(SampleTripperUserService.class);
+
     public void registerUser(String username, String password) {
+        LOGGER.info("User %s registered", username);
     }
 
     public void login(String username, String password) {
+        LOGGER.info("User %s logged in", username);
     }
 
     public void logout() {
+        LOGGER.info("User logged out");
     }
 }
