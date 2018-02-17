c411ff6146973a832a5481865cf1d60ca19bd4d6
Kush Chandna <kushchandna@gmail.com>
2018-02-17 13:55:04 +0530

    correcting existing services

--- tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperUserService.java
+++ tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperUserService.java
@@ -1,21 +1,27 @@
 package com.kush.tripper.sample.server;
 
 import com.kush.lib.service.server.api.BaseService;
+import com.kush.lib.service.server.api.annotations.Service;
+import com.kush.lib.service.server.api.annotations.ServiceMethod;
 import com.kush.logger.Logger;
 import com.kush.logger.LoggerFactory;
 
+@Service(name = "Sample Tripper User Service")
 public class SampleTripperUserService extends BaseService {
 
     private static final Logger LOGGER = LoggerFactory.INSTANCE.getLogger(SampleTripperUserService.class);
 
+    @ServiceMethod(name = "registerUser")
     public void registerUser(String username, String password) {
         LOGGER.info("User %s registered", username);
     }
 
+    @ServiceMethod(name = "login")
     public void login(String username, String password) {
         LOGGER.info("User %s logged in", username);
     }
 
+    @ServiceMethod(name = "logout")
     public void logout() {
         LOGGER.info("User logged out");
     }
