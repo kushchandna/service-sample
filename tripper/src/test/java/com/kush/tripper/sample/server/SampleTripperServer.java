9589a411b4f046a5aad145df5bd01207557122f0
Kush Chandna <kushchandna@gmail.com>
2018-02-28 15:13:33 +0530

    updated

--- tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperServer.java
+++ tripper/src/test/java/com/kush/tripper/sample/server/SampleTripperServer.java
@@ -1,9 +1,9 @@
 package com.kush.tripper.sample.server;
 
+import com.kush.lib.service.remoting.StartupFailedException;
 import com.kush.lib.service.server.ApplicationServer;
 import com.kush.lib.service.server.Context;
 import com.kush.lib.service.server.ContextBuilder;
-import com.kush.lib.service.server.ServiceInitializationFailedException;
 
 public class SampleTripperServer {
 
@@ -15,7 +15,7 @@ public class SampleTripperServer {
             .build();
         try {
             server.start(context);
-        } catch (ServiceInitializationFailedException e) {
+        } catch (StartupFailedException e) {
             e.printStackTrace();
         }
     }
