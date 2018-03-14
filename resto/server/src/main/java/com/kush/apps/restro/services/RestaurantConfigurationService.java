d8c9a3f59761f09157c4dff12ae4de0c6ee597da
Kush Chandna <kushchandna@gmail.com>
2018-03-15 00:49:51 +0530

    added service client generation in restro

---
+++ resto/server/src/main/java/com/kush/apps/restro/services/RestaurantConfigurationService.java
@@ -0,0 +1,8 @@
+package com.kush.apps.restro.services;
+
+import com.kush.lib.service.server.BaseService;
+import com.kush.lib.service.server.annotations.Service;
+
+@Service(name = "Restaurant Configuration")
+public class RestaurantConfigurationService extends BaseService {
+}
