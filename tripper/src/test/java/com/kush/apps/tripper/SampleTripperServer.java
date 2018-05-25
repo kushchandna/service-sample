246f20aaa8e94d2fee7f1b7671f155843684371b
Kush <Kush@KUSH>
2018-05-26 02:11:55 +0530

    using generic resolution

--- tripper/src/test/java/com/kush/apps/tripper/SampleTripperServer.java
+++ tripper/src/test/java/com/kush/apps/tripper/SampleTripperServer.java
@@ -11,14 +11,16 @@ import com.kush.apps.tripper.persistors.TripPlanPersistor;
 import com.kush.apps.tripper.services.TripPlannerService;
 import com.kush.lib.persistence.api.Persistor;
 import com.kush.lib.persistence.helpers.InMemoryPersistor;
-import com.kush.lib.service.remoting.StartupFailedException;
-import com.kush.lib.service.remoting.receiver.socket.ServerSocketServiceRequestReceiver;
+import com.kush.lib.service.remoting.ServiceRequest;
 import com.kush.service.ApplicationServer;
 import com.kush.service.Context;
 import com.kush.service.ContextBuilder;
 import com.kush.service.auth.credentials.DefaultUserCredentialPersistor;
 import com.kush.service.auth.credentials.UserCredential;
 import com.kush.service.auth.credentials.UserCredentialPersistor;
+import com.kush.utils.remoting.server.ResolvableProcessor;
+import com.kush.utils.remoting.server.StartupFailedException;
+import com.kush.utils.remoting.server.socket.SocketBasedResolvableProcessor;
 
 public class SampleTripperServer {
 
@@ -38,7 +40,7 @@ public class SampleTripperServer {
         ApplicationServer server = new ApplicationServer();
 
         Executor executor = Executors.newFixedThreadPool(3);
-        ServerSocketServiceRequestReceiver requestReceiver = new ServerSocketServiceRequestReceiver(executor, PORT);
+        ResolvableProcessor<ServiceRequest> requestReceiver = new SocketBasedResolvableProcessor<>(executor, PORT);
         server.registerServiceRequestReceiver(requestReceiver);
 
         server.registerService(TripPlannerService.class);
