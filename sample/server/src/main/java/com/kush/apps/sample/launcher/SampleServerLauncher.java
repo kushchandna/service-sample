4e24c9a7fa87e724644f5ba068c9777c72d27b9e
kushchandna <kushchandna@KUSH>
2020-10-20 00:51:38 +0530

    fixed some issues and unit tests

--- sample/server/src/main/java/com/kush/apps/sample/launcher/SampleServerLauncher.java
+++ sample/server/src/main/java/com/kush/apps/sample/launcher/SampleServerLauncher.java
@@ -1,19 +1,19 @@
 package com.kush.apps.sample.launcher;
 
-import static com.kush.lib.persistence.helpers.InMemoryPersistor.forType;
+import static com.kush.lib.persistence.helpers.InMemoryPersister.forType;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import com.kush.apps.sample.app.SampleHelloTextProvider;
 import com.kush.apps.sample.services.SampleHelloService;
-import com.kush.lib.persistence.api.Persistor;
+import com.kush.lib.persistence.api.Persister;
 import com.kush.service.ApplicationServer;
 import com.kush.service.Context;
 import com.kush.service.ContextBuilder;
-import com.kush.service.auth.credentials.DefaultUserCredentialPersistor;
+import com.kush.service.auth.credentials.DefaultUserCredentialPersister;
 import com.kush.service.auth.credentials.UserCredential;
-import com.kush.service.auth.credentials.UserCredentialPersistor;
+import com.kush.service.auth.credentials.UserCredentialPersister;
 import com.kush.utils.remoting.server.ResolutionRequestsReceiver;
 import com.kush.utils.remoting.server.StartupFailedException;
 import com.kush.utils.remoting.server.socket.SocketBasedResolutionRequestsProcessor;
@@ -36,10 +36,10 @@ public class SampleServerLauncher {
         ApplicationServer server = new ApplicationServer(serviceRequestReceiver);
         server.registerService(SampleHelloService.class);
         SampleHelloTextProvider greetingProvider = new SampleHelloTextProvider();
-        Persistor<UserCredential> delegate = forType(UserCredential.class);
+        Persister<UserCredential> delegate = forType(UserCredential.class);
         Context context = ContextBuilder.create()
             .withInstance(SampleHelloTextProvider.class, greetingProvider)
-            .withInstance(UserCredentialPersistor.class, new DefaultUserCredentialPersistor(delegate))
+            .withInstance(UserCredentialPersister.class, new DefaultUserCredentialPersister(delegate))
             .withInstance(SignalSpace.class, signalSpace)
             .build();
         server.start(context);
